16599c7a380f9eb013a292ce24167f72a244233a
Janhavi2705 <79911131+Janhavi2705@users.noreply.github.com>
2021-04-06 18:23:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bullet,wall,thickness;
+var speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50, 200, 50, 50);
+  bullet.shapeColor="white";
+  
+ //console.log(car)
+
+  wall=createSprite(1200,200,thickness,height/2);
+
+    speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(23,83);
+}
+
+function draw() {
+  background("black");  
+  bullet.velocityX=speed;
+
+
+if(hasCollieded(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+  if(damage>10)
+  {
+    wall.shapeColor=color(255,0,0);
+  }
+
+  if(damage<10)
+  {
+    wall.shapeColor=color(0,255,0);
+  }
+}
+  drawSprites();
+}
+
+function hasCollieded(lbullet,lwall){
+  bulletRightEdge=lbullet.x + lbullet.width;
+  wallLeftEdge=lwall.x;
+  if (bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+  return false;
+}
